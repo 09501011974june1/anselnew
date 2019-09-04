2b5ae64e84abba280933a584d3e4f330e93c30bc
09501011974june1 <juneansel@gmail.com>
2019-09-04 16:48:40 +0800

    ansel

---
+++ src/Controller/HelloWorldController.php
@@ -0,0 +1,18 @@
+<?php
+namespace Drupal\hello_world\Controller;
+
+class HelloWorldController{
+public function hello(){
+return array(
+'#title' =>'Hi team this is anselmo temple  ',
+'#markup' =>'This is my first module'
+
+);
+}
+}
+
+
+
+
+
+
